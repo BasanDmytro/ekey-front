50c56b5d9732b6bb720304f7a2f34edab893ab7c
Dmitriy Basan <basandima@gmail.com>
2016-12-19 08:39:28 +0200

    new message when connect to pc

--- serial/ser.js
+++ serial/ser.js
@@ -4,7 +4,6 @@ var app = express();
 var fs = require("fs");
 var SerialPort = serialport.SerialPort;
 
-
 var serialPort = new SerialPort("/dev/cu.usbmodem1411", {
   baudrate: 9600,
   parser: serialport.parsers.readline("\n")
